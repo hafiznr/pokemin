317d03fba26167bcd1e8236b6c1ca5850af392b2
hafiznr <hafiznr@gmail.com>
2021-05-30 13:38:48 +0700

    remove import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider } from '@apollo/client/react';
-import { ApolloClient, InMemoryCache } from '@apollo/client/core';
 import { BrowserRouter } from 'react-router-dom';
 import { renderRoutes } from 'react-router-config';
 import { ChakraProvider } from '@chakra-ui/react';
